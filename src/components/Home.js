7bad7ddceb6c21b9ba942c37e7e561a017ce1a82
Andrés Chaparro <66017759+Chappie21@users.noreply.github.com>
2021-10-15 12:47:07 -0400

    agregacion de prueba helppier

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,21 +5,30 @@ import ImgP from './ImgP';
 
 import { getImagesHome } from '../API/Image';
 
-import Refresh from '../img/refresh.svg' 
-
+import Refresh from '../img/refresh.svg'
+import help from '../img/help.svg'
 
 const Userprin = () => {
 
-    const query = useQuery('Home', getImagesHome,{
+    const query = useQuery('Home', getImagesHome, {
         staleTime: Infinity,
     });
 
     // Refrescar vista
-    const handleRefresh = () =>{
+    const handleRefresh = () => {
         query.refetch();
     }
 
-    if(query.isLoading){
+    // Cargar ayuda interactiva del feed
+    const handleChargeHelp = () => {
+        var helppierInit = window.helppierInit || function () { console.log('Error') };
+
+        helppierInit({ silent: true }, function (error, initFunctions) {
+            initFunctions.startPlay('LZZv9YlKW');
+        });
+    }
+
+    if (query.isLoading) {
         return (
             <div className='container-fluid position-relative vh-100'>
                 <div className="position-absolute top-50 start-50 translate-middle">
@@ -32,25 +41,32 @@ const Userprin = () => {
     }
 
     return (
-        <div className = 'container-fluid position-relative vh-100'>
-            <div className = 'd-flex justify-content-center mt-5 pt-5'>
-                <div className = 'col-12 col-sm-6'>
-                {
-                    query.data.data.publicaciones.map(pub =>{
-                        return(
-                           <div className = 'container mt-3 mb-3'>
-                                <ImgP username={pub.username} profileImg={pub.profileImg} descripcion={pub.descripccion} imgUrl={pub.pubUrl} tags={pub.tags}/>
-                            </div>
-                        )
-                    })
-                }
+        <div className='container-fluid position-relative vh-100'>
+            <div className='d-flex justify-content-center mt-5 pt-5'>
+                <div className='col-12 col-sm-6'>
+                    {
+                        query.data.data.publicaciones.map(pub => {
+                            return (
+                                <div className='container mt-3 mb-3'>
+                                    <ImgP username={pub.username} profileImg={pub.profileImg} descripcion={pub.descripccion} imgUrl={pub.pubUrl} tags={pub.tags} />
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
-            <div className = 'fixed-bottom pe-5 me-5 mb-5'>
-                    <button className = 'btn btn-primary float-sm-end rounded-pill'
-                        onClick = {handleRefresh}>
-                        <img src = {Refresh} alt='' width = '40px' height = '40px'/>
+            <div className='row d-flex'>
+                <div className='fixed-bottom pe-5 me-5 mb-5'>
+                    <button className='btn btn-primary ms-4 float-sm-start rounded-pill'
+                        onClick={handleChargeHelp}>
+                        <img src={help} alt='' width='40px' height='40px' />
                     </button>
+
+                    <button className='btn btn-primary float-sm-end rounded-pill'
+                        onClick={handleRefresh}>
+                        <img src={Refresh} alt='' width='40px' height='40px' />
+                    </button>
+                </div>
             </div>
         </div>
     );
